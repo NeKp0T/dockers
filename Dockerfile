1d2f1393141c6a65e8931d3a615c48ad872b012c
Vladilav Yepifanov <evn1999@yandex.ru>
2020-05-27 18:27:51 +0300

    workdi

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,9 @@ FROM python:3.5
 
 VOLUME ["/server"]
 ADD server /server
+WORKDIR /server
 EXPOSE 8000
 EXPOSE 8001
 
-CMD ["python3", "/server/server.py", "8000"]
+CMD ["python3", "./server.py", "8000"]
 # CMD ["python", "-m", "http.server", "8000"]
--- server/server.py
+++ server/server.py
@@ -7,12 +7,11 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        # self.wfile.write(b"qweqew\n")
         self.wfile.write(self.get_page())
 
     def get_page(self):
         res = BytesIO()
-        with open("/server/contents/main.html", "rb") as f:
+        with open("./contents/main.html", "rb") as f:
             while True:
                 buf = f.read(1024)
                 if buf: 
